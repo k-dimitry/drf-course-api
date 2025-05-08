75ef3c1f8ac34026822e953e079dd5f7fdc62c0d
Dimitry <tizya.noonie@gmail.com>
2025-05-08 20:40:28 +0300

    Finished part 12: JWT Authentication with djangorestframework-simplejwt

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'silk',
+    'rest_framework_simplejwt',
 
     'api',
 ]
@@ -112,3 +113,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'api.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
