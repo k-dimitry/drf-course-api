33745295900400e14762aee2fc6604c60c4c3667
Dimitry <tizya.noonie@gmail.com>
2025-05-08 12:09:35 +0300

    Finished part 4: Serializer subclasses and Aggregated API data

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from api.serializers import ProductSerializer, OrderSerializer
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from api.models import Product, Order
 
 
@@ -24,4 +25,15 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(data=serializer.data, status=200)
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price'],
+    })
     return Response(data=serializer.data, status=200)
